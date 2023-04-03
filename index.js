d95267138f4a9aeba957b23c3a605d80e387bef2
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-02 22:05:26 -0500

    connected to database

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+
+const pool =require("./sql/connections")
 const PORT = process.env.PORT || 5000;
 
 
 //route is the '/', and ()=>{} function is the controller
 app.get('/', (req,res)=> {
    res.json({message: "Hello Universe!"})
-
-
 });
+
+
+app.get('/users', (req,rews)=>{
+    pool.query("SELECT * FROM users", function(err, rows,fields) {
+        res.json(rows)
+    })
+});
+
 app.listen(PORT, ()=> console.log(`Listening @ http://localhost:${PORT}`));
