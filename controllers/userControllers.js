61eea7e142bbcc6d3d7dece29f49583c7885de54
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-03 21:50:47 -0500

    added user routes + controllers(MVC)

---
+++ controllers/userControllers.js
@@ -0,0 +1,59 @@
+const pool = require('../sql/connections');
+
+const list = (req,res)=>{
+    pool.query("SELECT * FROM users", function(err, rows,fields) {
+        res.json(rows)
+    })
+}
+ 
+ const show = (req,res)=> {
+    // console.log(req.params.id)
+    const {id} = req.params
+    pool.query(`SELECT * FROM users WHERE userId =${id}`, function(err, rows, fields) {
+   
+    res.json(rows)
+ }) 
+ };
+ 
+ const create = (req,res)=> {
+    // console.log(req.body)
+    
+    const {firstName, lastName, email, userName, password} = req.body
+ 
+    pool.query(`INSERT INTO users(userId, firstName, lastName, email, userName, password) VALUES (?, ?, ?, ?, ?, ?)`, 
+    [null, firstName, lastName, email, userName, password], 
+    function(err, row, fields) {
+   
+    res.json(row)
+ })
+ };
+ 
+const update = (req, res)=> {
+ 
+    const {userId} =req.params
+pool.query(`UPDATE users SET ? WHERE userId= ?`,
+    [req.body, userId],
+    function(err, row, fields) {
+        res.json(row)
+    })
+};
+  
+ 
+const remove = (req, res)=> {
+ 
+    const {userId} =req.params
+pool.query(`DELETE FROM users WHERE userId= ?`,
+    [userId],
+    function(err, row, fields) {
+        res.json(row)
+    })
+};
+
+
+module.exports = { 
+    list, 
+    show, 
+    create, 
+    update, 
+    remove
+};
--- index.js
+++ index.js
@@ -2,45 +2,22 @@ const express = require("express");
 const app = express();
 
 const pool =require("./sql/connections")
+
+const usersRoute =require("./routes/usersRoute")
 const PORT = process.env.PORT || 5000;
 
 
-//route is the '/', and ()=>{} function is the controller
+
+app.use(express.json());
+
+app.use('/users', usersRoute);
+
+ 
+
+
+
 app.get('/', (req,res)=> {
-   res.json({message: "Hello Universe!"})
-});
-
-
-app.get('/users', (req,res)=>{
-    pool.query("SELECT * FROM users", function(err, rows,fields) {
-        res.json(rows)
-    })
-});
-
-app.get('/users/:id', (req,res)=> {
-    // console.log(req.params.id)
-    const {id} = req.params
-    pool.query(`SELECT * FROM users WHERE userId =${id}`, function(err, rows, fields) {
-   
-    res.json(rows)
- }) 
+    res.json({message: "Hello Universe!"})
  });
-
- app.use(express.json());
-
- app.post('/users/', (req,res)=> {
-    console.log(req.body)
-    
-    const {firstName, lastName, email, userName, password} = req.body
- 
-    pool.query(`INSERT INTO users(userId, firstName, lastName, email, userName, password) VALUES (?, ?, ?, ?, ?, ?)`, 
-    [null, firstName, lastName, email, userName, password], 
-    function(err, row, fields) {
-   
-    res.json(row)
- })
- });
- 
- 
 
 app.listen(PORT, ()=> console.log(`Listening @ http://localhost:${PORT}`));
