916d82a413736c471e8bc6e88df39156e135479c
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-07 22:09:22 -0500

    added prettier

--- controllers/userLogsController.js
+++ controllers/userLogsController.js
@@ -1,144 +1,138 @@
-const pool = require('../sql/connections')
-
+const pool = require("../sql/connections");
 
 const list = (req, res) => {
-    const userId = parseInt(req.params.userId);
-  
-    pool.query(
-      `SELECT * FROM userLogs WHERE userId = ?`,
-      [userId],
-      function (err, rows, fields) {
-        if (err) {
-          console.log(err);
-          res.status(500).send('Error occurred while fetching userLogs');
-          return;
-        }
-        res.json(rows);
-      }
-    );
-  };
+  const userId = parseInt(req.params.userId);
 
-
-  const show = (req, res) => {
-    const userId = parseInt(req.params.userId);
-    const logId = parseInt(req.params.logId);
-  
-    pool.query(
-      `SELECT * FROM userLogs WHERE userId = ? AND logId = ?`,
-      [userId, logId],
-      function (err, row, fields) {
-        if (err) {
-          console.log(err);
-          res.status(500).send('Error occurred while fetching userLog');
-          return;
-        }
-        res.json(row[0]); // Send the first row since we expect a single log entry
+  pool.query(
+    `SELECT * FROM userLogs WHERE userId = ?`,
+    [userId],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error occurred while fetching userLogs");
+        return;
       }
-    );
-  };
+      res.json(rows);
+    }
+  );
+};
+
+const show = (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const logId = parseInt(req.params.logId);
+
+  pool.query(
+    `SELECT * FROM userLogs WHERE userId = ? AND logId = ?`,
+    [userId, logId],
+    function (err, row, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error occurred while fetching userLog");
+        return;
+      }
+      res.json(row[0]); // Send the first row since we expect a single log entry
+    }
+  );
+};
 
 const create = (req, res) => {
-    const { mood, userId } = req.body;
-  
-    // Find the moodId associated with the mood string
-    pool.query('SELECT moodId FROM moods WHERE moodName = ?', [mood], (err, rows, fields) => {
-      if (err) {
+  const { mood, userId } = req.body;
+
+  // Find the moodId associated with the mood string
+  pool.query(
+    "SELECT moodId FROM moods WHERE moodName = ?",
+    [mood],
+    (err, rows, fields) => {
+      if (err || rows.length === 0) {
         // Handle error
         console.log(err);
-        res.status(500).send('Error occurred while fetching moodId');
+        res.status(500).send("Error occurred while fetching moodId");
         return;
       }
-  
-      if (rows.length === 0) {
-        // If no matching mood is found, return an error message
-        res.status(400).send('Mood not found');
-        return;
-      }
-  
+
       // Get the moodId from the query result
       const moodId = rows[0].moodId;
-  
+
       // Insert a new record into the userLogs table
       const currentDate = new Date();
       pool.query(
-        'INSERT INTO userLogs(createDate, createTime, moodId, userId) VALUES (?, ?, ?, ?)',
+        "INSERT INTO userLogs(createDate, createTime, moodId, userId) VALUES (?, ?, ?, ?)",
         [currentDate, currentDate, moodId, userId],
         (err, result) => {
           if (err) {
             // Handle error
             console.log(err);
-            res.status(500).send('Error occurred while inserting userLog');
+            res.status(500).send("Error occurred while inserting userLog");
             return;
           }
-  
-          res.json({ message: 'UserLog created successfully' });
+
+          res.json({ message: "UserLog created successfully" });
         }
       );
-    });
-  };
-    
-
-  const update = (req, res) => {
-    const userId = parseInt(req.params.userId);
-    const logId = parseInt(req.params.logId);
-    const { mood } = req.body;
-  
-    if (isNaN(userId) || isNaN(logId)) {
-      res.status(400).send('Invalid userId or logId');
-      return;
     }
+  );
+};
 
-    // Find the moodId associated with the mood string
-    pool.query('SELECT moodId FROM moods WHERE moodName = ?', [mood], (err, rows, fields) => {
-      if (err || rows.length ===0) {
+const update = (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const logId = parseInt(req.params.logId);
+  const { mood } = req.body;
+
+  if (isNaN(userId) || isNaN(logId)) {
+    res.status(400).send("Invalid userId or logId");
+    return;
+  }
+
+  // Find the moodId associated with the mood string
+  pool.query(
+    "SELECT moodId FROM moods WHERE moodName = ?",
+    [mood],
+    (err, rows, fields) => {
+      if (err || rows.length === 0) {
         // Handle error
         console.log(err);
-        res.status(500).send('Error occurred while fetching moodId');
+        res.status(500).send("Error occurred while fetching moodId");
         return;
       }
-  
+
       // Get the moodId from the query result to update userLogs table
       const moodId = rows[0].moodId;
-  
-  
-  
-    pool.query(
-      `UPDATE userLogs SET moodId = ? WHERE userId = ? AND logId = ?`,
-      [moodId, userId, logId],
-      function (err, result) {
-        if (err) {
-          console.log(err);
-          res.status(500).send('Error occurred while updating the userLog');
-          return;
+
+      pool.query(
+        `UPDATE userLogs SET moodId = ? WHERE userId = ? AND logId = ?`,
+        [moodId, userId, logId],
+        function (err, result) {
+          if (err) {
+            console.log(err);
+            res.status(500).send("Error occurred while updating the userLog");
+            return;
+          }
+          res.json({ affectedRows: result.affectedRows });
         }
-        res.json({ affectedRows: result.affectedRows });
-      }
-    );
-  })};
+      );
+    }
+  );
+};
 
-const remove = (req,res)=> {
+const remove = (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const logId = parseInt(req.params.logId);
+  console.log(`userId: ${userId} and logId: ${logId}`);
 
-    const userId = parseInt(req.params.userId);
-    const logId = parseInt(req.params.logId);
-console.log(`userId: ${userId} and logId: ${logId}`)
-   
-pool.query(`DELETE FROM userLogs WHERE userId=? AND logId= ?`, 
+  pool.query(
+    `DELETE FROM userLogs WHERE userId=? AND logId= ?`,
     //put whatever updates that come in from req.body; could be 1 or all (i.e.  email, name, etc.) where id=id coming in
     [userId, logId],
-    function(err, row, fields) {
-    res.json(row)
- });
+    function (err, row, fields) {
+      res.json(row);
+    }
+  );
 };
 
-
-module.exports = { 
-    list, 
-    show, 
-    create, 
-    update, 
-    remove
+module.exports = {
+  list,
+  show,
+  create,
+  update,
+  remove,
 };
-
-
-
-
--- routes/userLogsRoute.js
+++ routes/userLogsRoute.js
@@ -1,12 +1,11 @@
-const express =require('express');
+const express = require("express");
 const router = express.Router();
-const userLogsController = require("../controllers/userLogsController")
+const userLogsController = require("../controllers/userLogsController");
 
-
-router.get('/:userId', userLogsController.list);
-router.get('/:userId/:logId', userLogsController.show);
-router.post('/', userLogsController.create);
-router.put('/:userId/:logId', userLogsController.update);
-router.delete('/:userId/:logId', userLogsController.remove);
+router.get("/:userId", userLogsController.list);
+router.get("/:userId/:logId", userLogsController.show);
+router.post("/", userLogsController.create);
+router.put("/:userId/:logId", userLogsController.update);
+router.delete("/:userId/:logId", userLogsController.remove);
 
 module.exports = router;
