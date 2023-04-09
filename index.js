df14df0512d8f9146ec834de4f2adc0430fb12c7
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-09 16:29:07 -0500

    authenticateToken middleware added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
 const app = express();
 
 const usersRoute =require("./routes/usersRoute");
@@ -10,15 +13,64 @@ const userInterventionRoute= require("./routes/userIntervention")
 const PORT = process.env.PORT || 5000;
 
 
+const authenticateToken=(req, res, next) => {
+    //Get meta information for our request
+    const authHeader = req.headers.authorization;
+    // console.log({auth: authHeader})
+
+   
+    //take the Bearer token (if it exists) and split it at the space and then take the 2nd item in array
+    const token = authHeader && authHeader.split(' ')[1];
+    // console.log(token);
+
+    //error 403 (not found) sent
+    if(!token) return res.sendStatus(403); 
+    
+    //if token exists, let's verify it. need to match token + our secret string (would usually hide the secret string in a .env)
+    // if first 2 args are good, then callback fxn fires
+    jwt.verify(token, 'tacos are good', (err, user)=> {
+        if(err) return res.sendStatus( 403);
+
+        // add property called user to the request object
+        // console.log({user})
+        // console.log(req)
+        req.user = user;
+
+    
+    });
+
+    next();
+}
+app.use(cors());
+app.use(function(req, res, next) {
+    //allow this url to access
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "http:localhost:5000"
+    );
+
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+
+    //request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    //this is for setting cookies
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+});
+
 
 app.use(express.json());
-app.use('/users', usersRoute);
-app.use('/userLogs', userLogsRoute);
 app.use('/signup', signupRoute);
 app.use('/signin', signinRoute);
-app.use('/interventions', userInterventionRoute);
-
-
+app.use('/users', authenticateToken, usersRoute);
+app.use('/userLogs', authenticateToken, userLogsRoute);
+app.use('/interventions', authenticateToken, userInterventionRoute);
 
 app.get('/', (req,res)=> {
     res.json({message: "Hello Universe!"})
