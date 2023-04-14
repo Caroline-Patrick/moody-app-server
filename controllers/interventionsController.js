3cfaade50127d9c2bae4d73053e31a8f41d6d801
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-14 14:20:28 -0500

    added query to join user made interventions + preset interventions to populate together

--- controllers/interventionsController.js
+++ controllers/interventionsController.js
@@ -1,22 +1,71 @@
 const pool = require("../sql/connections");
 
-const list = (req, res) => {
-  const userId = parseInt(req.params.userId);
-  const subSubMoodId = parseInt(req.params.subSubMoodId)
+// const list = (req, res) => {
+//   const userId = parseInt(req.params.userId);
+//   const subSubMoodId = parseInt(req.params.subSubMoodId)
 
+//   pool.query(
+//     `SELECT *
+//     FROM userInterventions
+//     JOIN mood_interventions
+//       ON userInterventions.moodInterventionId = mood_interventions.id
+//     WHERE userInterventions.userId = ? AND mood_interventions.subSubMoodId = ?
+//     `,
+//     [userId, subSubMoodId],
+//     function (err, rows, fields) {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Error occurred while fetching interventions");
+//         return;
+//       }
+//       res.json(rows);
+//     }
+//   );
+// };
+
+
+const list = (req, res) => {
+  const {userId, subSubMoodId} = req.params;
   pool.query(
-    `SELECT * FROM mood_interventions WHERE userId = ?`,
-    [userId],
+    'SELECT * FROM userInterventionMoods JOIN mood_interventions ON userInterventionMoods.subSubMoodId = mood_interventions.subSubMoodId WHERE userInterventionMoods.subSubMoodId = ? OR mood_interventions.subSubMoodId = ?',
+    [subSubMoodId, subSubMoodId],
     function (err, rows, fields) {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error occurred while fetching userInterventions");
-        return;
-      }
-      res.json(rows);
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error occurred while fetching interventions");
+            return;
+        }
+        res.json(rows);
     }
-  );
-};
+);
+
+  // pool.query(
+  //       `SELECT *
+  //       FROM mood_interventions
+  //       JOIN userInterventionMoods
+  //         ON mood_interventions.subSubMoodId = userInterventionMoods.subSubMoodId
+  //       JOIN userInterventions
+  //         ON userInterventionMoods.userInterventionId = userInterventions.userInterventionId
+  //       WHERE userInterventions.userId = ? AND mood_interventions.subSubMoodId = ?
+  //       `,
+  //         [userId, subSubMoodId],
+  //         (err, rows, fields) => {
+  //           if (err) {
+  //             console.log(err);
+  //             res.status(500).send("Error occurred while fetching interventions");
+  //             return;
+  //           }
+  
+  //           // Send a response with the intervention options
+
+  //           console.log(rows)
+  //           res.json({
+  //             message: "Complete",
+  //             interventions: rows,
+  //           });
+  //         }
+  //       );
+      }
 
 
 const show = (req, res) => {
