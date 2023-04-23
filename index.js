d31a24fd6a24c801092b2400abbb247fc7c82bb2
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-23 17:41:05 -0500

    added netlify link

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const authenticateToken = (req, res, next) => {
 app.use(cors());
 app.use(function (req, res, next) {
   //allow this url to access
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://effulgent-strudel-007716.netlify.app");
 
   // Request methods you wish to allow
   res.setHeader("Access-Control-Allow-Methods", "POST");
