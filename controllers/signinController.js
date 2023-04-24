03fbfb650996d5aa74c44aa0b2d8d6c881fcd7dc
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-23 19:15:03 -0500

    changing signincontroller socket

--- controllers/signinController.js
+++ controllers/signinController.js
@@ -5,9 +5,8 @@ const jwt = require('jsonwebtoken');
 const generateToken = (user) => {
     return jwt.sign(user, 'tacos are good');
 };
-
 const signin = (req, res) => {
-    res.json({req})
+    
     const { email, password } = req.body;
     console.log("Email and password received:", email, password);
   
@@ -38,6 +37,39 @@ const signin = (req, res) => {
         }
       });
   };
+  
+// const signin = (req, res) => {
+    
+//     const { email, password } = req.body;
+//     console.log("Email and password received:", email, password);
+  
+//     pool.query(`SELECT * FROM users WHERE email='${email}'`,
+//       async (err, results, fields) => {
+//         if (err) {
+//           console.error("Error in query execution:", err);
+//           return res.status(500).json({ error: "Internal server error" });
+//         }
+  
+//         console.log("Query results:", results);
+  
+//         if (results.length === 0) {
+//           return res.status(404).json({ error: "User not found" });
+//         }
+  
+//         const match = await bcrypt.compare(password, results[0].password);
+//         console.log("Password match:", match);
+  
+//         if (match) {
+//           const token = generateToken(results[0]);
+//           res.json({
+//             token,
+//             user: results[0],
+//           });
+//         } else {
+//           res.status(401).json({ error: "Invalid password" });
+//         }
+//       });
+//   };
   
 
 module.exports = {
